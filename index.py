4b758be7d045dce0288d5f3abe75856be0add683
kainatfatima1605 <kainatrafiq1625@gmail.com>
2025-01-16 11:09:59 -0800

    whilee loop

--- index.py
+++ index.py
@@ -14,25 +14,33 @@
 #   boolen = False
 #   print(boolen)
 #   print("yes you are right")
-islamic_names = [
-    "Abdullah", "AbdulRahman", "Aisha", "Ahmed", "Ali", "Amir", "Ammar", "Anas", "Ayaan", "Bilal",
-    "Dania", "Ebrahim", "Ehsan", "Fatima", "Fawad", "Hafsa", "Hamza", "Hassan", "Haya", "Huzaifa",
-    "Imran", "Isa", "Ishaq", "Jannah", "Jibril", "Khadija", "Khalid", "Layla", "Lubna", "Mariam",
-    "Maryam", "Mohammed", "Musa", "Nabeel", "Nida", "Noor", "Omar", "Osman", "Rahil", "Rahman",
-    "Rayyan", "Rida", "Sabrina", "Saif", "Saima", "Sana", "Sarah", "Shaheer", "Shoaib", "Sufyan",
-    "Sumayya", "Taha", "Talha", "Usman", "Uzair", "Yahya", "Yusuf", "Zain", "Zainab", "Zubair",
-    "Aadil", "Abbas", "Adnan", "Afra", "Amna", "Asim", "Asiya", "Ayat", "Basil", "Basit",
-    "Burhan", "Dawood", "Emaan", "Fareed", "Farhan", "Fazal", "Ghufran", "Haleema", "Haroon", "Huda",
-    "Ibrahim", "Idrees", "Ikram", "Iqra", "Irfan", "Jamal", "Jawad", "Kaif", "Kamil", "Kanza",
-    "Lamees", "Mahad", "Maham", "Mahnoor", "Mansoor", "Maria", "Mehar", "Moin", "Mueez", "Mumtaz",
-    "Nafees", "Nahyan", "Najwa", "Nasir", "Nashit", "Nawal", "Nimrah", "Rameez", "Rania", "Rashid",
-    "Rehan", "Rimsha", "Rubina", "Saad", "Sabeen", "Sadaf", "Sadia", "Sami", "Sanaullah", "Sarah",
-    "Shifa", "Sobia", "Sohail", "Subhan", "Tabassum", "Tanzeela", "Tasneem", "Wafa", "Waleed", "Wasay",
-    "Yumna", "ZainulAbideen", "Zayd", "Zoha", "Zohra", "Zulekha", "Abrar", "Adeeb", "Ahsan", "Asad",
-    "Bashir", "Bushra", "Darim", "Fahad", "Faisal", "Ghazi", "Habib", "Haseeb", "Huzayfah", "Izzah",
-    "Javeria", "Kaif", "Khawar", "Lubaba", "Mahira", "Malik", "Mashal", "Mifrah", "Muawiya", "Mudassir",
-    "Nashrah", "Nayab", "Noman", "Nuha", "Rizwan", "Rukhsana", "Saima", "Salahuddin", "Saleha", "Salim",
-    "Shanaya", "Sohail", "Sumrah", "Tanzeem", "Uzma", "Waqar", "Warda", "Yasmin", "Yusra", "Zarah"
-]
-for i in islamic_names:
-    print(i)
+# islamic_names = [
+
+#     "Abdullah", "AbdulRahman", "Aisha", "Ahmed", "Ali", "Amir", "Ammar", "Anas", "Ayaan", "Bilal",
+#     "Dania", "Ebrahim", "Ehsan", "Fatima", "Fawad", "Hafsa", "Hamza", "Hassan", "Haya", "Huzaifa",
+#     "Imran", "Isa", "Ishaq", "Jannah", "Jibril", "Khadija", "Khalid", "Layla", "Lubna", "Mariam",
+#     "Maryam", "Mohammed", "Musa", "Nabeel", "Nida", "Noor", "Omar", "Osman", "Rahil", "Rahman",
+#     "Rayyan", "Rida", "Sabrina", "Saif", "Saima", "Sana", "Sarah", "Shaheer", "Shoaib", "Sufyan",
+#     "Sumayya", "Taha", "Talha", "Usman", "Uzair", "Yahya", "Yusuf", "Zain", "Zainab", "Zubair",
+#     "Aadil", "Abbas", "Adnan", "Afra", "Amna", "Asim", "Asiya", "Ayat", "Basil", "Basit",
+#     "Burhan", "Dawood", "Emaan", "Fareed", "Farhan", "Fazal", "Ghufran", "Haleema", "Haroon", "Huda",
+#     "Ibrahim", "Idrees", "Ikram", "Iqra", "Irfan", "Jamal", "Jawad", "Kaif", "Kamil", "Kanza",
+#     "Lamees", "Mahad", "Maham", "Mahnoor", "Mansoor", "Maria", "Mehar", "Moin", "Mueez", "Mumtaz",
+#     "Nafees", "Nahyan", "Najwa", "Nasir", "Nashit", "Nawal", "Nimrah", "Rameez", "Rania", "Rashid",
+#     "Rehan", "Rimsha", "Rubina", "Saad", "Sabeen", "Sadaf", "Sadia", "Sami", "Sanaullah", "Sarah",
+#     "Shifa", "Sobia", "Sohail", "Subhan", "Tabassum", "Tanzeela", "Tasneem", "Wafa", "Waleed", "Wasay",
+#     "Yumna", "ZainulAbideen", "Zayd", "Zoha", "Zohra", "Zulekha", "Abrar", "Adeeb", "Ahsan", "Asad",
+#     "Bashir", "Bushra", "Darim", "Fahad", "Faisal", "Ghazi", "Habib", "Haseeb", "Huzayfah", "Izzah",
+#     "Javeria", "Kaif", "Khawar", "Lubaba", "Mahira", "Malik", "Mashal", "Mifrah", "Muawiya", "Mudassir",
+#     "Nashrah", "Nayab", "Noman", "Nuha", "Rizwan", "Rukhsana", "Saima", "Salahuddin", "Saleha", "Salim",
+#     "Shanaya", "Sohail", "Sumrah", "Tanzeem", "Uzma", "Waqar", "Warda", "Yasmin", "Yusra", "Zarah"
+# ]
+# for i in islamic_names:
+#      print(i)
+
+mystudents =["kainat","hira","eman","fatima","ilsa"]
+count = len(mystudents)-1
+while count >=0:
+    print(count,mystudents[count])
+    count = count -1
+
